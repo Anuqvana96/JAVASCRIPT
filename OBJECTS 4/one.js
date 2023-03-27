9fd4be5134d84d886bc96027464d86d1ed06bcad
Anuqvana96 <anuqvana9606@gmail.com>
2023-03-27 19:37:01 +0530

    first

---
+++ OBJECTS 4/one.js
@@ -0,0 +1,28 @@
+
+//object
+
+let Person={
+
+    name:"Anu",
+    age:22,
+    gender:"Female",
+    address:"Bangalore,karnataka",
+    siblings:{
+        brothers:"sakthi,vetri"
+    }
+   
+}
+ //object
+
+ Person.age=23
+
+ //Dot Notation
+
+ console.log(Person.name)
+ console.log(Person.age)
+ console.log(Person.address)
+ console.log(Person.siblings)
+ 
+ //Bracket Notation
+
+ console.log(Person['gender'])
---
+++ datatype_3/one.js
@@ -0,0 +1,10 @@
+
+let firstname="ANU";  //string
+
+let age=22;  //Integer/Number
+
+let isyoung=true  //Boolean
+
+let lastname=undefined;  //undefined
+
+let tropy=null  //Null
