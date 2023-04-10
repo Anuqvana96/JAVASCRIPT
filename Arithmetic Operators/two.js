71a55bd0beaf0d5da9b007ecdd66514d431735fd
Anuqvana96 <anuqvana9606@gmail.com>
2023-04-10 17:03:01 +0530

    anu

---
+++ Arithmetic Operators/two.js
@@ -0,0 +1,5 @@
+let number1=4;
+let number2=2;
+let result=number1 + number2
+console.log(result);
+
---
+++ Arrays_5/two.js
@@ -0,0 +1,7 @@
+
+
+let favcolors=["red","white","black"]  //Arrays
+
+favcolors[4]=45
+
+console.log(favcolors[4])
